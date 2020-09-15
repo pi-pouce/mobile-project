2e610235aed7261c5d4856860a1562477a572d16
pi-pouce <71259173+pi-pouce@users.noreply.github.com>
2020-09-15 09:35:53 +0200

    init project configuration cli

---
+++ swift-project-with-cli-configuration/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swift-project-with-cli-configuration
+//
+//  Created by Tarik on 15/09/2020.
+//  Copyright © 2020 pi-pouce. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
